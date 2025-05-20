547e502ac0233a0ef30e49c1295cc3d7940c4939
Xavione <92642568+Xavione@users.noreply.github.com>
2025-05-19 21:20:37 -0500

    Add FrequencyMap.java example

---
+++ hashmaps/FrequencyMap.java
@@ -0,0 +1,20 @@
+import java.util.HashMap;
+
+public class FrequencyMap {
+    public static void main(String[] args) {
+        int[] nums = {2, 3, 2, 4, 3, 2};
+        HashMap<Integer, Integer> map = new HashMap<>();
+
+        for (int num : nums) {
+            if (map.containsKey(num)) {
+                map.put(num, map.get(num) + 1);
+            } else {
+                map.put(num, 1);
+            }
+        }
+
+        for (int key : map.keySet()) {
+            System.out.println(key + ": " + map.get(key));
+        }
+    }
+}
